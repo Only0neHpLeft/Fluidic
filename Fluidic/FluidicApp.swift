f67a5ca808a6daae678c9aa88bd3526b5a01e043
Filip_Kabyl <onlydmgleft@gmail.com>
2026-02-22 11:44:05 +0100

    Initial Commit

---
+++ Fluidic/FluidicApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluidicApp.swift
+//  Fluidic
+//
+//  Created by Only0neHpLeft on 22.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct FluidicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FluidicTests/FluidicTests.swift
@@ -0,0 +1,17 @@
+//
+//  FluidicTests.swift
+//  FluidicTests
+//
+//  Created by Only0neHpLeft on 22.02.2026.
+//
+
+import Testing
+@testable import Fluidic
+
+struct FluidicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
